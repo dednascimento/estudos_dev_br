239f8df1749ee71cb3b9544eff73901306fc051d
DedN4 <acslegendados16@gmail.com>
2023-10-13 15:56:59 -0300

    Informações adicionadas - Aula 006
    Feat: informações adicionadas relacionadas a nova sintaxe python.

--- estudos_python/ex002.py
+++ estudos_python/ex002.py
@@ -2,3 +2,12 @@
 
 nome = input("Olá, diga seu nome ?")
 print("Olá, " + nome)
+
+
+# Será substituido pela nova sintaxe abaixo:
+
+print("Olá, {}".format(nome))
+
+#  ----- Explicação
+# {} = Onde o valor da variável irá entrar
+# .formart(nome_variavel_utilizada)

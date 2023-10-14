f46c08aae53b0965aac22f15cc5a31bc4cb96d91
DedN4 <acslegendados16@gmail.com>
2023-10-14 15:15:40 -0300

    Conclusão do Exercício 009
    - Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
    - Aprendi a procurar métodos mais práticos, iremos retornar a esse
      exercício após o próximo módulo

---
+++ estudos_python/ex009.py
@@ -0,0 +1,22 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+n = int(input("Digite o número que você saber a tabuda: "))
+
+print("O número digitado foi {}, a tabela abaixo irá mostrar á respeito da sua tabuada: ".format(n))
+
+print("="*12)
+print("{} x {:2} = {}".format(n, 1, n*1))
+print("{} x {:2} = {}".format(n, 2, n*2))
+print("{} x {:2} = {}".format(n, 3, n*3))
+print("{} x {:2} = {}".format(n, 4, n*4))
+print("{} x {:2} = {}".format(n, 5, n*5))
+print("{} x {:2} = {}".format(n, 6, n*6))
+print("{} x {:2} = {}".format(n, 7, n*7))
+print("{} x {:2} = {}".format(n, 8, n*8))
+print("{} x {:2} = {}".format(n, 9, n*9))
+print("{} x {:2} = {}".format(n, 10, n*10))
+print("="*12)
+
+
+# Tinha feito de um jeito que ficou mais confuso, mas após o ajuste aparentemente ficou bom.
+# Concluído

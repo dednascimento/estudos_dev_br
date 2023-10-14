fa623e274e82a77598f36f7233876cfd1e550492
DedN4 <acslegendados16@gmail.com>
2023-10-14 13:52:25 -0300

    Conclusão do Exercício 005
    Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
    Feito.

---
+++ estudos_python/ex005.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input("Digite um número: "))
+s = n+1
+a = n-1
+
+print("O número digitado foi {}, tendo como sucessor {}, e o ser acessor é {}".format(n, s, a))
+
+# Concluído

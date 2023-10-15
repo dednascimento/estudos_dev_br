55a70a1b85ec1847cabe13f3e711691551f1a4ea
DedN4 <acslegendados16@gmail.com>
2023-10-15 01:10:11 -0300

    Conclusão do Exercício - Conversão de Dolar/Euro para Real
    - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
    - Feito a adesão do Euro pois ele mencionou para nos desafiramos e achei
      interessante, logo fiz
    - Achei fácil o processo

---
+++ estudos_python/curso_em_video/modulo001/ex010.py
@@ -0,0 +1,10 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+
+real = float(input("\nDigite quantos reais você deseja investir com a compra: \n- R$"))
+dolar = 5.08
+euro = 5.34
+
+real_dolar = real/dolar
+real_euro = real/euro
+
+print("\n- Valor do Dolar atualizado: {}US$. \n- Valor do Euro atualizado: {}€. \n- Seu saldo: R${}.\n\nVocê consegue comprar: \n{:.2f}US$ (Dolares) \n{:.2f}€ (Euros) \n".format(dolar, euro, real, real_dolar, real_euro))

475701cdb04aaf827ab58d05ead3e366f2d40c46
DedN4 <acslegendados16@gmail.com>
2023-10-17 11:48:25 -0300

    Conclusão do Exercício 017 - Hipotenusa
    Aprendi o calculo da hipotenusa em python
    Só que ele lançou um desafio usando a biblioteca
    Irei atualizar

---
+++ estudos_python/curso_em_video/modulo005/ex017.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
+
+co = float(input("Digite o cateto oposto do triângulo retangulo! "))
+ca = float(input("Digite o cateto adjacente do triângulo retangulo! "))
+
+hp = (co ** 2 + ca ** 2) ** (1/2)
+
+print("Levando em conta que: \nCateto oposto: {}\nCateto adjacente: {}\n--> O valor da hipotenusa á {:.2f}".format(co, ca, hp))
+
+# hipotenusa² = cateto_oposto² + cateto_adjacente²
+

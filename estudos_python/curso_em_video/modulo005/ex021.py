ae4f5679977b4b12ca9876fb0e521fff0014c1cc
DedN4 <acslegendados16@gmail.com>
2023-10-19 12:09:07 -0300

    Conclusão do exercício - PyGame(Music)

--- estudos_python/curso_em_video/modulo005/ex021.py
+++ estudos_python/curso_em_video/modulo005/ex021.py
@@ -1,1 +1,7 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('./estudos_python/curso_em_video/modulo005/ex021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait(10)

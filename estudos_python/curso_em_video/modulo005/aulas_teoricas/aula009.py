c3b3b2bdb4692f1bb1e8c86701095c81d1962c08
DedN4 <acslegendados16@gmail.com>
2023-10-22 12:20:56 -0300

    Iniciando - Manipulação de Texto (PY)

---
+++ estudos_python/curso_em_video/modulo005/aulas_teoricas/aula009.py
@@ -0,0 +1,2 @@
+# Nessa aula, vamos aprender operações com String no Python. As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
+

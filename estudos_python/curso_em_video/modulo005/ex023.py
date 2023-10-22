950995da9615e7f4527e247f6159136353a8c2d3
DedN4 <acslegendados16@gmail.com>
2023-10-22 16:16:58 -0300

    Conclusão - Separando dígitos de um número (PY)
    - Separando dígitos de um número
    - Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
    - Concluído - Mas não cheguei ao resultado pois não estava usando a lógica matematica, tentei chegar ao resultado usando puramente String e divisão de texto

---
+++ estudos_python/curso_em_video/modulo005/ex023.py
@@ -0,0 +1,20 @@
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+
+num = int(input('\nDigite um número de 0 á 9999: \nR: '))
+
+
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+# 1 2 3 4
+# m c d u
+
+print("\nAnálisando o número {}...\n...\n... \n".format(num))
+print("Unidade: {}".format(u))
+print("Dezena: {}".format(d))
+print("Centena: {}".format(c))
+print("Milhar: {}\n\n".format(m))
+
+# Concluído - Mas não cheguei ao resultado pois não estava usando a lógica matematica, tentei chegar ao resultado usando puramente String e divisão de texto

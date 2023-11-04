b141d1e8c6f78eafb6086f6d5e8668d4831ebf9a
DedN4 <acslegendados16@gmail.com>
2023-11-04 11:56:18 -0300

    Conclusão da Aula - Dados[2/4]

---
+++ estudos_node/mod03/dados2.js
@@ -0,0 +1,9 @@
+let produto = "Caderno"
+let categoria = "Papelaria"
+let preco = 19.90
+
+console.log(
+    "O produto escolhido foi: " + produto
+ + ", Que se encontra na categoria: " + categoria
+ + ", custando apenas: " + preco
+ );

3c18eb74015a234654afd13e572cac04cff2983a
DedN4 <acslegendados16@gmail.com>
2023-11-04 11:43:14 -0300

    Conclusão da Aula sobre Dados [1/2']

---
+++ estudos_node/mod03/dados1.js
@@ -0,0 +1,19 @@
+// Criação de váriaveis JS
+
+var nome = "Caneta";
+var preco = 6.7;
+var imposto = 1.5;
+var valor_total = preco + imposto;
+
+console.log(nome);
+console.log(preco);
+console.log(imposto);
+console.log(valor_total);
+
+
+// Abaixo estão números os items no valor iteral, no caso valor iteral é quando o item é aplicado diretamente no JS
+
+console.log("Caneta")
+console.log(6.7)
+console.log(1.5)
+console.log(8.2)

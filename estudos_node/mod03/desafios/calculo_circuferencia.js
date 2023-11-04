c444e991596d29f6c9e79a04e43ff9b780940c5a
Deivid N <100328493+dednascimento@users.noreply.github.com>
2023-11-04 17:32:50 -0300

    Conclusão - Desafio de Area

---
+++ estudos_node/mod03/desafios/calculo_circuferencia.js
@@ -0,0 +1,8 @@
+// Calculo de Raio
+const PI = 3.14 // Valor de PI é uma constante matemática 
+const raio = 10 // Valor do Raio é variável
+calcRaio = (raio * raio) * PI // PI * Raio * Raio
+console.log("O valor da área é de: " + calcRaio + "m²")
+
+areaCirc = Math.PI * raio * raio; // Math.PI - Exibir o valor de PIO segundo a matematica
+console.log("O valor da área é de: " + areaCirc + "m²")

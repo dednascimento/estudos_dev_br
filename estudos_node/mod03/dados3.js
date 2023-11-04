8fb9424beac3285391c319d9684e1e0c4604d885
Deivid N <100328493+dednascimento@users.noreply.github.com>
2023-11-04 16:55:31 -0300

    Conclusão Aula - 'Dados 03'

---
+++ estudos_node/mod03/dados3.js
@@ -0,0 +1,17 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.42;
+console.log(typeof salario);
+
+// Diferente do Pyhton, mesmo o determinado número tendo ponto, ele continua sendo 'Number' não tem versões diferentes
+
+let estaChovendo = true // ou false
+console.log(typeof estaChovendo);
+// Valores Booleanos (Verdadeiro, Falso, Certo, Incorreto, Desligado...
+
+console.log(typeof "Teste");
+// String = Conjunto de Simbolos
+
+// Conclusão Aula - 'Dados 03'

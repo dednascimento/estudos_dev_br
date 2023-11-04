0cb177132690f4c0c3c20003eaa6e3a4fe52f19d
Deivid N <100328493+dednascimento@users.noreply.github.com>
2023-11-04 19:51:55 -0300

    Conclusão - Exercício de Inversão

---
+++ estudos_node/mod03/desafios/desafio_invetesão.js
@@ -0,0 +1,20 @@
+// -----------------------------------------------
+
+let a = 7; let b = 94
+
+a = 94;
+b = 7; 
+// Meio de troca muito usado
+
+console.log(a);
+console.log(b);
+
+// -----------------------------------------------
+
+[a, b] = [b, a] 
+// Quando os dados são aleatórios e não podem ser previsos
+
+console.log(a); 
+console.log(b); 
+
+// -----------------------------------------------

bc984a6bacf4ed1eb0c41045be0ebbad3479fd2e
DedN4 <acslegendados16@gmail.com>
2023-11-06 12:02:32 -0300

    Alguns observações do tipo 'Number'

---
+++ estudos_node/mod04/cuidados.js
@@ -0,0 +1,22 @@
+
+
+
+console.log(7/0)
+/* 
+ * Na matemática, quanto mais se aproximamos de uma divisão por 0, mais casas ele terá. Nesse caso o JS tem um próprio tipo para esse tipo de valor 
+ *
+ * Type 'Infinity' para casos assim 
+
+*/
+
+console.log("10"/2)
+/*
+ * Se caso o console detectar
+ * que a String que estivermos tentando
+ * usar na operação aritmética conter apenas
+ * Númneros, ele irá fazer a divisão normalmente
+ * .... MAS SE APENAS CONTER NÚMEROS
+ */ 
+
+// console.log(10.toString())
+console.log((10).toString()) // Para converter para string, nunca utilize o número diretamente

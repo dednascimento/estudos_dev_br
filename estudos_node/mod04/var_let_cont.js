a6121d90a7c4cc59878f63ce2b325b2405329d64
DedN4 <acslegendados16@gmail.com>
2023-11-06 11:11:23 -0300

    Conclusão da Aula (Let, Var e Const)

---
+++ estudos_node/mod04/var_let_cont.js
@@ -0,0 +1,19 @@
+// A diferença entre esses atributos será exibida mais á frente
+{
+    var b = 4;
+    let a = 3; // Método mais usado atualmente
+    const c = 5; // Valor constante não sofre alterações
+    console.log(a, b, c)
+}
+
+
+// Aparemente não podemos atribuir valores que não estejam dentro de blocos
+
+
+// Para atribuir novamente usamos os meios abaixo
+{
+    a = 30
+    var b = 40
+    console.log(a, b)
+}
+

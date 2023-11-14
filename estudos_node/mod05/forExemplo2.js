b9cb1c08a1cac7cdda38c3fb36b5290e4fada480
DedN4 <acslegendados16@gmail.com>
2023-11-14 18:56:03 -0300

    Conclusão do exercício sobre For com Array

---
+++ estudos_node/mod05/forExemplo2.js
@@ -0,0 +1,17 @@
+const notas = [5.4, 6.4, 7.2, 8.8, 9.4, 10.0]
+
+// 'i < 'notas.length' = Do ponto inicial até o máximo de elementos que possuem no array.
+
+for (let i = 0; i < notas.length; i++) {
+    console.log(`Notas = ${notas[i]}`)
+}
+
+/* 
+    No caso aqui, utilizamos um 'Array' com as notas e vamos exibir todas conforme o texto.
+
+    - notas[i] = imprimir o array 'notas' conforme o número que for atribuido á 'i'
+
+    Ou seja, se 'i' for 0 = '5.4' (Elemento do índice 0)
+
+    Criamos a váriavel 'i' e definimos como '0' pois o 'Array' sempre começa com elemento '0'
+*/

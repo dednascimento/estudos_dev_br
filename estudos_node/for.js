d184d2980f7f1102e8576d0751a7fbfa56ecf45c
DedN4 <acslegendados16@gmail.com>
2023-11-14 16:01:47 -0300

    Conclusão For

---
+++ estudos_node/for.js
@@ -0,0 +1,17 @@
+let contador = 1
+
+// Enquando a condição 'while' for verdadeira ele não irá parar de executar.
+
+while (contador <= 10) {
+    console.log(`Contagem em ${contador}`)
+    contador++
+}
+
+
+for (let num = 1; num <= 10; num++) {
+    console.log(num)   
+}
+
+// 'For' trás bastante vantagem para usarmos em situações no qual precisamos de um limite, ele tem esse foco e é mais simples para isso
+
+// Não significa que somente isso podemos fazer.

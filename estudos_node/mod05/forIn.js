b378197731b6b5afae321a6c4aace0ced0795a95
DedN4 <acslegendados16@gmail.com>
2023-11-27 09:48:29 -0300

    Exercício node

--- estudos_node/mod05/forIn.js
+++ estudos_node/mod05/forIn.js
@@ -3,3 +3,14 @@ const notas = [6.6, 7.2, 8.8, 9.4, 10.0]
 for (i in notas) {
     console.log(`${i} ${notas[i]}`)
 }
+
+const pessoa = {
+    nome: 'Deivid',
+    sobrenome: "Nascimento",
+    idade: 20,
+    peso: 69.9,
+}
+
+for (let atributos in pessoa) {
+    console.log(`${atributos} = ${pessoa[atributos]}`)
+}
